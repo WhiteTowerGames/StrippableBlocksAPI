d542e55d037f993d2ac0dcd8f3deb0053e2fb9d8
Christos Maragkos <eco23140@uom.edu.gr>
2025-02-16 12:45:21 +0200

    Idk what to write

--- src/main/java/com/strippableblocksapi/mixin/ExampleMixin.java
+++ src/main/java/com/strippableblocksapi/mixin/ExampleMixin.java
@@ -1,15 +1,36 @@
 package com.strippableblocksapi.mixin;
 
+import com.strippableblocksapi.StrippableCustomRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
+@Mixin(AxeItem.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+
+	@Inject(method = "useOnBlock", at = @At("HEAD"), cancellable = true)
+	private void onUse(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+
+		World world = context.getWorld();
+		BlockPos pos = context.getBlockPos();
+		Block block = world.getBlockState(pos).getBlock();
+
+		Block strippedBlock = StrippableCustomRegistry.getStripppedResult(block);
+
+		if (strippedBlock != null){
+			world.setBlockState(pos, strippedBlock.getDefaultState());
+			cir.setReturnValue(ActionResult.SUCCESS);
+		}
+
 	}
+
 }
