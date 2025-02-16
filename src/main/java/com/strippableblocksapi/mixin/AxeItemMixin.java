115ebab0c8aa51474e27e15f180fd7dc9438c2f6
Christos Maragkos <eco23140@uom.edu.gr>
2025-02-16 14:37:49 +0200

    Added stripping sounds

--- src/main/java/com/strippableblocksapi/mixin/AxeItemMixin.java
+++ src/main/java/com/strippableblocksapi/mixin/AxeItemMixin.java
@@ -4,18 +4,23 @@ import com.strippableblocksapi.StrippableCustomRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemUsageContext;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AxeItem.class)
-public class ExampleMixin {
+public class AxeItemMixin {
+
+	@Unique
+	public SoundEvent getStripSound() {return SoundEvents.ITEM_AXE_STRIP;}
 
 	@Inject(method = "useOnBlock", at = @At("HEAD"), cancellable = true)
 	private void onUse(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
@@ -28,6 +33,7 @@ public class ExampleMixin {
 
 		if (strippedBlock != null){
 			world.setBlockState(pos, strippedBlock.getDefaultState());
+			world.playSound(null, pos, getStripSound(), SoundCategory.BLOCKS, 1.0F, 1.0F);
 			cir.setReturnValue(ActionResult.SUCCESS);
 		}
 
