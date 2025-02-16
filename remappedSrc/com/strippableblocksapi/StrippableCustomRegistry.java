dcaedbd66592173e26af650ca5f159ecda905ba3
Christos Maragkos <eco23140@uom.edu.gr>
2025-02-16 18:54:34 +0200

    Created 1.21.4 port

---
+++ remappedSrc/com/strippableblocksapi/StrippableCustomRegistry.java
@@ -0,0 +1,20 @@
+package com.strippableblocksapi;
+
+import net.minecraft.block.Block;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StrippableCustomRegistry {
+
+    private static final Map<Block, Block> STRIPPABLES = new HashMap<>();
+
+    public static void register(Block input, Block output){
+        STRIPPABLES.put(input, output);
+    }
+
+    public static Block getStripppedResult(Block input){
+        return STRIPPABLES.get(input);
+    }
+
+}
