753d861952f0258df210c764ea577d118a1f53fe
Christos Maragkos <eco23140@uom.edu.gr>
2025-02-27 00:13:07 +0200

    idk what to write

--- src/main/java/com/strippableblocksapi/StrippableBlocksAPI.java
+++ src/main/java/com/strippableblocksapi/StrippableBlocksAPI.java
@@ -2,23 +2,17 @@ package com.strippableblocksapi;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class StrippableBlocksAPI implements ModInitializer {
 	public static final String MOD_ID = "strippableblocksapi";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static boolean isPedestalsLoaded = false;
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		isPedestalsLoaded = FabricLoader.getInstance().isModLoaded("pedestals");
 	}
 }
