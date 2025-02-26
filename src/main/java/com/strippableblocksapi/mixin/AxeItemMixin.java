158ec8aee38c41268487e601133e586d52b339fb
Christos Maragkos <eco23140@uom.edu.gr>
2025-02-27 00:27:45 +0200

    idk what to write

--- src/main/java/com/strippableblocksapi/mixin/AxeItemMixin.java
+++ src/main/java/com/strippableblocksapi/mixin/AxeItemMixin.java
@@ -1,8 +1,12 @@
 package com.strippableblocksapi.mixin;
 
+import com.strippableblocksapi.StrippableBlocksAPI;
 import com.strippableblocksapi.StrippableCustomRegistry;
+import net.chris.pedestals.block.entity.PedestalBlockEntity;
 import net.minecraft.block.Block;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
@@ -22,6 +26,7 @@ public class AxeItemMixin {
 	@Unique
 	public SoundEvent getStripSound() {return SoundEvents.ITEM_AXE_STRIP;}
 
+
 	@Inject(method = "useOnBlock", at = @At("HEAD"), cancellable = true)
 	private void onUse(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
 
@@ -32,11 +37,23 @@ public class AxeItemMixin {
 		Block strippedBlock = StrippableCustomRegistry.getStrippedResult(block);
 
 		if (strippedBlock != null){
-			world.setBlockState(pos, strippedBlock.getDefaultState());
+			BlockEntity blockEntity = world.getBlockEntity(pos);
+
+			if (StrippableBlocksAPI.isPedestalsLoaded) {
+				if (blockEntity instanceof PedestalBlockEntity pedestalBlockEntity) {
+					ItemStack currentItem = pedestalBlockEntity.getStoredItem();
+					world.setBlockState(pos, strippedBlock.getDefaultState(), 3);
+					PedestalBlockEntity newPedestalBlockEntity = (PedestalBlockEntity) world.getBlockEntity(pos);
+					assert newPedestalBlockEntity != null;
+					newPedestalBlockEntity.setStoredItem(currentItem);
+				}
+			} else {
+				world.setBlockState(pos, strippedBlock.getDefaultState(), 3);
+			}
+
 			world.playSound(null, pos, getStripSound(), SoundCategory.BLOCKS, 1.0F, 1.0F);
 			cir.setReturnValue(ActionResult.SUCCESS);
+
 		}
-
 	}
-
 }
